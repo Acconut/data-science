1a16fef2f9f93e9161d30ccf92e36eb4757f347d
Acconut <maerious@gmail.com>
2018-06-10 15:24:55 +0200

    Checkin

--- news_dnn.py
+++ news_dnn.py
@@ -1,20 +1,33 @@
-
 # coding: utf-8
-
-# In[1]:
-
-
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.neural_network import MLPClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score, f1_score
+from sklearn.datasets import fetch_20newsgroups
+
+
+
+# Stemming Code
+
+# import nltk
+# nltk.download('stopwords')
+
+# from nltk.stem.snowball import SnowballStemmer
+# stemmer = SnowballStemmer("english", ignore_stopwords=True)
+
+# class StemmedCountVectorizer(CountVectorizer):
+#     def build_analyzer(self):
+#         analyzer = super(StemmedCountVectorizer, self).build_analyzer()
+#         return lambda doc: ([stemmer.stem(w) for w in analyzer(doc)])
+
+import numpy as np
 
 #Loading the data set - training data.
-from sklearn.datasets import fetch_20newsgroups
-twenty_train = fetch_20newsgroups(subset='train', shuffle=True)
-import numpy as np
-twenty_test = fetch_20newsgroups(subset='test', shuffle=True)
+twenty_train = fetch_20newsgroups(subset='train', remove=('headers','footers'), shuffle=True)
+twenty_test = fetch_20newsgroups(subset='test', remove=('headers','footers'), shuffle=True)
 
 print 'Training...'
 
@@ -23,29 +36,35 @@ print 'Training...'
 # Building a pipeline: We can write less code and do all of the above, by building a pipeline as follows:
 # The names ‘vect’ , ‘tfidf’ and ‘clf’ are arbitrary but will be used later.
 # We will be using the 'text_clf' going forward.
-from sklearn.pipeline import Pipeline
-from sklearn.model_selection import GridSearchCV
 
 param_grid = {
-	'clf__hidden_layer_sizes': [(10), (50), (100)]
+	'vect__ngram_range': [(1,1), (1,2), (1,3)],
+	#'vect__ngram_range': [(1,2)],
+	'vect__min_df': [1, 3, 5]
+	#'clf__hidden_layer_sizes': [(50)]#[(10), (50), (100)]
 }
 
 pipe = Pipeline([
-	('vect', CountVectorizer()),
+	('vect', CountVectorizer(
+		stop_words='english'
+	)),
 	('tfidf', TfidfTransformer()),
 	('clf', MLPClassifier(
-		max_iter=10,
-		#solver='lbfgs',
-		#hidden_layer_sizes=(50),
-		verbose=True
+		#max_iter=10,
+		solver='adam',
+		hidden_layer_sizes=(100),
+		verbose=True,
+		random_state=0,
+		early_stopping=True,
+		tol=0.001
 	))
 ])
 
 grid = GridSearchCV(pipe,
 	param_grid=param_grid,
-	cv=5,
+	cv=4,
 	verbose=1,
-	n_jobs=7
+	#n_jobs=4
 )
 
 text_clf = grid.fit(twenty_train.data, twenty_train.target)
