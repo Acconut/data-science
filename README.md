8f62156fd5f7fe7674a4dab02aa12cafd35a43f2
Acconut <maerious@gmail.com>
2018-08-31 23:11:07 +0200

    Clean up code

---
+++ README.md
@@ -0,0 +1,23 @@
+# Introduction
+
+This repository contains the code for the experiment associated with the topic "Hyperparameter Study for Classical Feed-forward Neural Networks on Text Data".
+
+# Installation
+
+This experiment was carried out using Python 2.7.14.
+In order to install the necessary Python packages, use the commands below:
+
+```
+pip install scikit-learn==0.19.1
+pip install numpy==1.14.3
+pip install pandas==0.23.0
+
+```
+
+# Usage
+
+Running the experiment is as simple as passing the script to Python:
+
+```
+python news.py
+```
--- news.py
+++ news.py
@@ -9,31 +9,27 @@ from sklearn.metrics import accuracy_score, f1_score
 from sklearn.datasets import fetch_20newsgroups
 
 import numpy as np
+import pandas as pd
 
-#Loading the data set - training data.
+# Loading the training and testing data set
 twenty_train = fetch_20newsgroups(subset='train', remove=('headers','footers'), shuffle=True)
 twenty_test = fetch_20newsgroups(subset='test', remove=('headers','footers'), shuffle=True)
 
 print 'Training...'
 
-# In[14]:
-
-# Building a pipeline: We can write less code and do all of the above, by building a pipeline as follows:
-# The names ‘vect’ , ‘tfidf’ and ‘clf’ are arbitrary but will be used later.
-# We will be using the 'text_clf' going forward.
-
+# Define the hyperparameters that we want to try out in the grid search
 param_grid = {
 	'clf__hidden_layer_sizes': [(50), (100), (50, 50), (100, 100)],
 	'clf__learning_rate_init': [1, 0.1, 0.01, 0.001, 0.0001]
 }
 
+# Setup a pipeline to define the preprocessing and feature extraction steps
 pipe = Pipeline([
 	('vect', CountVectorizer(
 		stop_words='english'
 	)),
 	('tfidf', TfidfTransformer()),
 	('clf', MLPClassifier(
-		#max_iter=10,
 		solver='adam',
 		verbose=True,
 		random_state=0,
@@ -42,6 +38,7 @@ pipe = Pipeline([
 	))
 ])
 
+# Setup the parameters for the grid search
 grid = GridSearchCV(pipe,
 	param_grid=param_grid,
 	cv=4,
@@ -49,18 +46,21 @@ grid = GridSearchCV(pipe,
 	n_jobs=7
 )
 
+# Do the actual training and fit the classifier to the training set
 text_clf = grid.fit(twenty_train.data, twenty_train.target)
 
 print 'Done!'
 
+# Print out information about the best classifier after training
 print "Best cross-validation accuracy", grid.best_score_
-print "Test best score", grid.score(twenty_test.data, twenty_test.target)
 print "Best parameters", grid.best_params_
 
-# In[15]:
+results = pd.DataFrame(grid_search.cv_results_)
+scores = np.array(results.mean_test_score).reshape(4, 5)
+print "Scores for all parameters", scores
 
-# Performance of NB Classifier
+# Evaluate the best classifier based on the unseen test data
 predicted = text_clf.predict(twenty_test.data)
-print "Accuracy:", accuracy_score(twenty_test.target, predicted)
-print "Macro F1:", f1_score(twenty_test.target, predicted, average='macro')  
-print "Micro F1:", f1_score(twenty_test.target, predicted, average='micro') 
+print "Accuracy on test data:", accuracy_score(twenty_test.target, predicted)
+print "Macro F1 on test data:", f1_score(twenty_test.target, predicted, average='macro')  
+print "Micro F1 on test data:", f1_score(twenty_test.target, predicted, average='micro')
